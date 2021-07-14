b4a6880be990024bfeb0c6c99d02478645d4e2b6
Timothy Stone <tim.stone@kinsaleins.com>
2021-07-14 11:29:01 -0400

    Commit initial project files from resources
    The course files were not provided as a git repo and this corrects this
    gap.
    
    Added a .gitignore and converted all line endings to Unix, from DOS.
    
    master will represent the project at completion. Branches will follow a
    begin and end format for each module.

---
+++ src/main/java/com/pluralsight/tddjunit5/airport/Airport.java
@@ -0,0 +1,27 @@
+package com.pluralsight.tddjunit5.airport;
+
+public class Airport {
+
+    public static void main(String[] args) {
+        Flight economyFlight = new Flight("1", "Economy");
+        Flight businessFlight = new Flight("2", "Business");
+
+        Passenger john = new Passenger("John", true);
+        Passenger mike = new Passenger("Mike", false);
+
+        businessFlight.addPassenger(john);
+        businessFlight.removePassenger(john);
+        businessFlight.addPassenger(mike);
+        economyFlight.addPassenger(mike);
+
+        System.out.println("Business flight passengers list:");
+        for (Passenger passenger: businessFlight.getPassengersList()) {
+            System.out.println(passenger.getName());
+        }
+
+        System.out.println("Economy flight passengers list:");
+        for (Passenger passenger: economyFlight.getPassengersList()) {
+            System.out.println(passenger.getName());
+        }
+    }
+}
