4293fb0e9229522d96d2bccfffb193188964c80d
Timothy Stone <tim.stone@kinsaleins.com>
2021-07-15 09:55:08 -0400

    Refactor for more JUnit 5 features
    Building on some optimization from Module 2 that was perform, the class
    further nests the tests by flight and passenger. More use of DisplayName
     annotations to follow Given/When/Then. Use assertAll to group tests and
      simplify assertions throughout.

--- src/test/java/com/pluralsight/tddjunit5/airport/AirportTest.java
+++ src/test/java/com/pluralsight/tddjunit5/airport/AirportTest.java
@@ -1,76 +1,103 @@
 package com.pluralsight.tddjunit5.airport;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class AirportTest {
 
-  private Flight economyFlight;
-  private Flight businessFlight;
   private Passenger typical = new Passenger("Mike", false);
   private Passenger vip = new Passenger("John", true);
 
+  @DisplayName("Given there is an Economy Flight")
   @Nested
   class EconomyFlightTests {
+    private Flight economyFlight;
 
     @BeforeEach
     void setUp() {
       economyFlight = new EconomyFlight("1");
     }
 
-    @Test
-    void testEconomyFlightWithTypicalPassenger() {
-
-      assertEquals("1", economyFlight.getId());
-      assertEquals(true, economyFlight.addPassenger(typical));
-      assertEquals(1, economyFlight.getPassengersList().size());
-      assertEquals("Mike", economyFlight.getPassengersList().get(0).getName());
-
-      assertEquals(true, economyFlight.removePassenger(typical));
-      assertEquals(0, economyFlight.getPassengersList().size());
+    @DisplayName("When we have a typical passenger")
+    @Nested
+    class TypicalPassenger {
+      @DisplayName("Then you can add and remove the passenger from an Economy Flight")
+      @Test
+      void testEconomyFlightWithTypicalPassenger() {
+        assertAll("Verify all conditions for a typical passenger and an economy flight",
+            () -> assertEquals("1", economyFlight.getId()),
+            () -> assertTrue(economyFlight.addPassenger(typical)),
+            () -> assertEquals(1, economyFlight.getPassengersList().size()),
+            () -> assertEquals("Mike", economyFlight.getPassengersList().get(0).getName()),
+            () -> assertTrue(economyFlight.removePassenger(typical)),
+            () -> assertEquals(0, economyFlight.getPassengersList().size())
+        );
+      }
     }
 
-    @Test
-    void testEconomyFlightWithVipPassenger() {
-
-      assertEquals("1", economyFlight.getId());
-      assertTrue(economyFlight.addPassenger(vip));
-      assertEquals(1, economyFlight.getPassengersList().size());
-      assertEquals("John", economyFlight.getPassengersList().get(0).getName());
-
-      assertFalse(economyFlight.removePassenger(vip));
-      assertEquals(1, economyFlight.getPassengersList().size());
+    @DisplayName("When we have a VIP Passenger")
+    @Nested
+    class VipPassenger {
+      @DisplayName("Then you can add, but not remove, the passenger from an Economy Flight")
+      @Test
+      void testEconomyFlightWithVipPassenger() {
+        assertAll("Verify all conditions for a VIP passenger and an economy flight",
+            () -> assertEquals("1", economyFlight.getId()),
+            () -> assertTrue(economyFlight.addPassenger(vip)),
+            () -> assertEquals(1, economyFlight.getPassengersList().size()),
+            () -> assertEquals("John", economyFlight.getPassengersList().get(0).getName()),
+            () -> assertFalse(economyFlight.removePassenger(vip)),
+            () -> assertEquals(1, economyFlight.getPassengersList().size())
+        );
+      }
     }
   }
 
+  @DisplayName("Given there is an business flight")
   @Nested
   class BusinessFlightTests {
+    private Flight businessFlight;
 
     @BeforeEach
     void setUp() {
       businessFlight = new BusinessFlight("2");
     }
 
-    @Test
-    void testBusinessFlightWithTypicalPassenger() {
-      assertFalse(businessFlight.addPassenger(typical));
-      assertEquals(0, businessFlight.getPassengersList().size());
-      assertFalse(businessFlight.removePassenger(typical));
-      assertEquals(0, businessFlight.getPassengersList().size());
+    @DisplayName("When we have a typical passenger")
+    @Nested
+    class TypicalPassenger {
+      @DisplayName("Then you cannot add or remove the passenger from a Business Flight")
+      @Test
+      void testBusinessFlightWithTypicalPassenger() {
+        assertAll("Verify all conditions for a typical passenger and a business flight",
+            () -> assertFalse(businessFlight.addPassenger(typical)),
+            () -> assertEquals(0, businessFlight.getPassengersList().size()),
+            () -> assertFalse(businessFlight.removePassenger(typical)),
+            () -> assertEquals(0, businessFlight.getPassengersList().size())
+        );
+      }
     }
 
-    @Test
-    void testBusinessFlightWithVipPassenger() {
-      assertTrue(businessFlight.addPassenger(vip));
-      assertEquals(1, businessFlight.getPassengersList().size());
-      assertFalse(businessFlight.removePassenger(vip));
-      assertEquals(1, businessFlight.getPassengersList().size());
+    @DisplayName("When we have a VIP passenger")
+    @Nested
+    class VipPassenger {
+      @DisplayName("Then you can add, but not remove, the passenger from a Business Flight")
+      @Test
+      void testBusinessFlightWithVipPassenger() {
+        assertAll("Verify all conditions for a VIP passenger and a business flight",
+            () -> assertTrue(businessFlight.addPassenger(vip)),
+            () -> assertEquals(1, businessFlight.getPassengersList().size()),
+            () -> assertFalse(businessFlight.removePassenger(vip)),
+            () -> assertEquals(1, businessFlight.getPassengersList().size())
+        );
+      }
     }
-
   }
 }
